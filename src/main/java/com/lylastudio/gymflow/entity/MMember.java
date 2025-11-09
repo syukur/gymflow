c7cfb1c428bedc0473d45a5032d15d8484a57a11
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2025-11-10 06:43:27 +0700

    refactor Member to MMember 1

--- src/main/java/com/lylastudio/gymflow/entity/MMember.java
+++ src/main/java/com/lylastudio/gymflow/entity/MMember.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class gi MMember {
+public class MMember {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
