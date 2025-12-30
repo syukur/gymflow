e9e6dbd2895c637a1e6de9cfe0b126553cde4e55
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2025-12-30 08:14:37 +0700

    setup all entity : add table t_class_schedule

---
+++ src/main/java/com/lylastudio/gymflow/entity/TClassSchedule.java
@@ -0,0 +1,44 @@
+package com.lylastudio.gymflow.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "t_class_schedule")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TClassSchedule extends  BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    // Foreign Key ke Branch (sisi Many)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trainer_id", nullable = false)
+    private MTrainer trainer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_type_id", nullable = false)
+    private MClassType classType;
+
+    @Column(name = "capacity")
+    private int capacity;
+
+    @Column(name = "start_training")
+    private LocalDateTime startTraining;
+
+    @Column(name = "end_training")
+    private LocalDateTime endTraining;
+
+    @Column(name = "description", length = 100)
+    private String description;
+
+
+
+}
