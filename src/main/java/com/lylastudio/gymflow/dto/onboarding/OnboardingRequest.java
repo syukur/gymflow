fa954b8f73ea73ac667cb79b5689fc3391c41cd0
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-01-11 10:41:25 +0700

    onboarding : add sweager documentation

--- src/main/java/com/lylastudio/gymflow/dto/onboarding/OnboardingRequest.java
+++ src/main/java/com/lylastudio/gymflow/dto/onboarding/OnboardingRequest.java
@@ -1,5 +1,6 @@
 package com.lylastudio.gymflow.dto.onboarding;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
@@ -15,22 +16,27 @@ import java.util.List;
 @AllArgsConstructor
 public class OnboardingRequest {
 
+    @Schema(description = "Company Name")
     @NotBlank(message = "{validation.required}")
     @Size(min = 3, max = 50, message = "{validation.length}")
     private String companyName;
 
+    @Schema(description = "Company mobile phone number, (recomendation: whatsapp number)")
     @NotBlank(message = "{validation.phone.required}")
     @Pattern(regexp = "^[0-9]+$", message = "{validation.phone.pattern}")
     @Size(min = 10, max = 20, message = "{validation.phone.length}")
     private String phoneNumber;
 
+    @Schema(description = "Company Currency")
     @NotBlank(message = "{validation.required}")
     @Size(min = 3, max = 3, message = "{validation.length}")
     private String currency;
 
+    @Schema(description = "List of package")
     @Valid
     private List<Package> packages;
 
+    @Schema(description = "List of branch")
     @Valid
     private List<Branch> branches;
 
@@ -40,14 +46,17 @@ public class OnboardingRequest {
     @AllArgsConstructor
     public static class Package {
 
+        @Schema(description = "Package Name")
         @NotBlank(message = "{validation.required}")
         @Size(min = 3, max = 50, message = "{validation.length}")
         private String name;
 
+        @Schema(description = "Package Price")
         @NotNull(message = "{validation.required}")
         @PositiveOrZero(message = "{validation.must.number}")
         private Double price;
 
+        @Schema(description = "Package Duration")
         @NotNull(message = "{validation.required}")
         @PositiveOrZero(message = "{validation.must.number}")
         private Integer duration; // Duration in days or months, depending on your logic
@@ -59,10 +68,12 @@ public class OnboardingRequest {
     @AllArgsConstructor
     public static class Branch {
 
+        @Schema(description = "Branch Name")
         @NotBlank(message = "{validation.required}")
         @Size(min = 3, max = 50, message = "{validation.length}")
         private String name;
 
+        @Schema(description = "Branch Address")
         @NotBlank(message = "{validation.required}")
         @Size(min = 3, max = 50, message = "{validation.length}")
         private String address;
