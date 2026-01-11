8361fc0122f1d41fd9ae7b37cde3679ec49e3703
syukur.kurnia@dwidasa.com <syukur.kurnia@dwidasa.com>
2026-01-11 10:00:45 +0700

    onboarding validation

--- src/main/java/com/lylastudio/gymflow/dto/onboarding/OnboardingRequest.java
+++ src/main/java/com/lylastudio/gymflow/dto/onboarding/OnboardingRequest.java
@@ -1,5 +1,7 @@
 package com.lylastudio.gymflow.dto.onboarding;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,10 +15,23 @@ import java.util.List;
 @AllArgsConstructor
 public class OnboardingRequest {
 
+    @NotBlank(message = "{validation.required}")
+    @Size(min = 3, max = 50, message = "{validation.length}")
     private String companyName;
+
+    @NotBlank(message = "{validation.phone.required}")
+    @Pattern(regexp = "^[0-9]+$", message = "{validation.phone.pattern}")
+    @Size(min = 10, max = 20, message = "{validation.phone.length}")
     private String phoneNumber;
+
+    @NotBlank(message = "{validation.required}")
+    @Size(min = 3, max = 3, message = "{validation.length}")
     private String currency;
+
+    @Valid
     private List<Package> packages;
+
+    @Valid
     private List<Branch> branches;
 
     @Data
@@ -24,8 +39,17 @@ public class OnboardingRequest {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Package {
+
+        @NotBlank(message = "{validation.required}")
+        @Size(min = 3, max = 50, message = "{validation.length}")
         private String name;
+
+        @NotNull(message = "{validation.required}")
+        @PositiveOrZero(message = "{validation.must.number}")
         private Double price;
+
+        @NotNull(message = "{validation.required}")
+        @PositiveOrZero(message = "{validation.must.number}")
         private Integer duration; // Duration in days or months, depending on your logic
     }
 
@@ -34,7 +58,13 @@ public class OnboardingRequest {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Branch {
+
+        @NotBlank(message = "{validation.required}")
+        @Size(min = 3, max = 50, message = "{validation.length}")
         private String name;
+
+        @NotBlank(message = "{validation.required}")
+        @Size(min = 3, max = 50, message = "{validation.length}")
         private String address;
     }
 }
